775ae5d4b80be655056fccd167d3cac7c40ce76a
carlosmyr23 <carlosmyr23@gmail.com>
2022-11-11 10:40:59 -0600

    Update Analisis de precios.py

--- Analisis de precios.py
+++ Analisis de precios.py
@@ -1,4 +1,4 @@
-import time
+from unittest.util import strclass
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -7,11 +7,21 @@ from selenium.webdriver.support import expected_conditions as ec
 import re
 from bs4 import BeautifulSoup as BS
 import requests
+# más codigo
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-PATH = "D:/Federico/Proyectos GIT/Proyectos/Selenium-Test/chromedriver.exe"
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+PATH = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 busqueda = input('Especifique que desea buscar en ML: ').lower()
-url = 'https://www.mercadolibre.com.ar/'
+url = 'https://www.mercadolibre.com.mx/'
 busqueda_split = busqueda.split(' ')
 
 #
@@ -21,40 +31,57 @@ chrome_options.add_argument("--headless")
 
 # XPath:
 
-path_busqueda = '/html/body/header/div/form/input'
-boton_busqueda = '/html/body/header/div/form/button'
+#path_busqueda = '/html/body/header/div/form/input'
+#boton_busqueda = '/html/body/header/div/form/button'
+
+path_busqueda = '//*[@id="cb1-edit"]'
+boton_busqueda = '/html/body/header/div/form/button/div'
+
 
 # Abrir navegador
 
-driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome('/home/carlos/tests/chromedriver')
 
 # Maximizar pantalla
-
 driver.maximize_window()
-
 driver.get(url)
 
+
+
 # Acciones en la web
+#driver.find_element_by_xpath(path_busqueda).send_keys(busqueda)
+#driver.find_element_by_xpath(boton_busqueda).click()
 
-driver.find_element_by_xpath(path_busqueda).send_keys(busqueda)
-driver.find_element_by_xpath(boton_busqueda).click()
+#driver.find_element(By.XPATH, path_busqueda)
+#driver.find_element(By.XPATH, boton_busqueda).click()
 
-time.sleep(3)
+# by class busqueda automatica
+
+time.sleep(10)
+clase = "nav-search-input"
+driver.find_element(By.CLASS_NAME, clase).send_keys(busqueda)
+icono = "nav-icon-search"
+driver.find_element(By.CLASS_NAME, icono).click()
+time.sleep(10)
 
 link_ml = str(driver.current_url)
+
 
 # Creacion de archivo csv y escritura de encabezados
 archivo_out = open('consulta '+busqueda+'.csv', 'w', encoding='utf-8')
 archivo_out.write(link_ml+'\n')
 archivo_out.write('Titulo,Precio,Cantidad,Ventas,Vendedor,Link\n')
 
-# Eextraccion del html
+#Extraccion del html
 html = driver.execute_script('return document.documentElement.outerHTML')
 dom = BS(html, features='html.parser')
 
+#___________________________________________-
+
+
 # extraccion de las distintas publicaciones listadas
 publicaciones = dom.find_all('div', class_='ui-search-result__content-wrapper')
-
 
 # iteracion de extraccion de datos
 for publicacion in range(len(publicaciones)):
@@ -87,6 +114,8 @@ for publicacion in range(len(publicaciones)):
             pos_final = precio_busq.find('</')
             precio = (precio_busq[pos+2:pos_final])
             precio = precio.replace('.', '')
+            precio = precio.replace(',', '')
+
             precio = str(precio)
             print(precio)
 
@@ -125,8 +154,28 @@ for publicacion in range(len(publicaciones)):
             print(titulo+','+precio+','+cantidad+','+ventas+','+vendedor+','+link+'\n')
             print('ahora si')
 
+
+
+
     except:
         continue
+
+domnose = open('domnose.txt', 'w')
+htmltexto = open('htmltexto.txt', 'w')
+html2 = html
+html2 = str(html2)
+dom2 = dom 
+dom2 = str(dom)
+domnose.write( str(html2) ) # + '\n'
+domnose.close()
+htmltexto.write( str(dom2) ) # + '\n'
+htmltexto.close()
+
+
+salida_html_dom = open('consulta '+busqueda+'.html', 'w', encoding='utf-8')
+salida_html_dom.write( str(dom2) )
+salida_html_dom.close()
+
 
 archivo_out.close()
 print('Se creo el archivo .csv correctamente')
